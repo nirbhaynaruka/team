f016dda8211458c3c5b8ffe54507333f7071d6c1
nirbhaynaruka <49834120+nirbhaynaruka@users.noreply.github.com>
2019-09-06 04:05:52 +0530

    j

--- js/home.js
+++ js/home.js
@@ -16,3 +16,16 @@ $(function(){
             $('div.box1, div.box2').toggle();
         });
 });  
+$(function(){
+    $('div.sudhanshu').hide();
+        $('.toggle3').on('click', function(){
+            $('div.nirbhay, div.sudhanshu').toggle();
+        });
+});  
+$(function(){
+    $('div.something').hide();
+        $('.toggle4').on('click', function(){
+            $('div.sudhanshu, div.nirbhay').toggle();
+        });
+});  
+
